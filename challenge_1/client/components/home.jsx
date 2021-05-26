0c4771b58c901b16a331488252f53a84dc9ff1ef
Chloe Tan <chloe.tan95@gmail.com>
2021-05-25 22:43:18 -0400

    Complete adding search textbox and button to front end

--- challenge_1/client/components/home.jsx
+++ challenge_1/client/components/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchForm from './searchForm.jsx';
 
 class Home extends React.Component {
   constructor(props) {
@@ -6,7 +7,12 @@ class Home extends React.Component {
   }
 
   render() {
-    return <h1>Hello World!</h1>
+    return (
+      <div>
+        <h1>Historical Events Finder</h1>
+        <SearchForm />
+      </div>
+    )
   }
 }
 
---
+++ challenge_1/client/components/searchForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchForm = (props) => {
+  return (
+    <form class="search-form">
+      <div class="search-query">
+        <label for="searchInput">Search for historical events: </label>
+        <input type="text" name="searchInput" id="search-input" required></input>
+        <input type="submit" value="Search"></input>
+      </div>
+    </form>
+  )
+}
+
+export default SearchForm;
