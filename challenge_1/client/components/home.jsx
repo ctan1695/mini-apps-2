f29610ebb03cab909c4dea896bca2c9add33d58d
Chloe Tan <chloe.tan95@gmail.com>
2021-05-25 23:29:36 -0400

    Connect front-end text search with json-server

--- challenge_1/client/components/home.jsx
+++ challenge_1/client/components/home.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import SearchForm from './searchForm.jsx';
+
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchResults: []
+    };
+
+    this.clickSearch = this.clickSearch.bind(this);
+  }
+
+  clickSearch(event) {
+    event.preventDefault();
+
+    let searchValue = event.target[0].value;
+
+    axios.get(`http://localhost:3000/events?q=${searchValue}`)
+      .then((res) => {
+        this.setState({searchResults: res.data});
+        console.log('res.data: ', res.data);
+      })
+      .catch((err) => {
+        console.log(`Error calling GET for full-text search: ${err}`);
+      })
   }
 
   render() {
     return (
       <div>
         <h1>Historical Events Finder</h1>
-        <SearchForm />
+        <SearchForm handleSearch={this.clickSearch}/>
       </div>
     )
   }
--- challenge_1/client/components/searchForm.jsx
+++ challenge_1/client/components/searchForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchForm = (props) => {
   return (
-    <form class="search-form">
+    <form class="search-form" onSubmit={props.handleSearch}>
       <div class="search-query">
         <label for="searchInput">Search for historical events: </label>
         <input type="text" name="searchInput" id="search-input" required></input>
