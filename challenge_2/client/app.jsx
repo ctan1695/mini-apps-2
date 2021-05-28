ba90743c258709276f6a1269fc24b86acf6634ef
Chloe Tan <chloe.tan95@gmail.com>
2021-05-28 11:38:25 -0400

    Complete basic setup of react, webpack and express

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const targetElement = document.getElementById('app');
+
+ReactDOM.render(<h1>hi world</h1>, targetElement);
 
 
+console.log('targetElement: ', targetElement)
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3000;
+const path = require('path');
+const distPath = `${__dirname}/public`;
 
+app.use(express.static(distPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  console.log('hi: ', path.join(distPath, 'index.html'))
+  res.sendFile(path.join(distPath, 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+})
 
