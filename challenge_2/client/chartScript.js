6c8080b7c5aeedbf53cdf7d19c97f30cce9e7b6f
Chloe Tan <chloe.tan95@gmail.com>
2021-05-30 21:27:20 -0400

    Complete BMR

--- challenge_2/client/chartScript.js
+++ challenge_2/client/chartScript.js
@@ -14,11 +14,11 @@ const getDateLabels = (begin, end) => {
     return result;
 };
 
-const dateRange = getDateLabels(new Date(2021, 00, 1), new Date(2021, 00, 03));
+const dateRange = getDateLabels(new Date(2021, 00, 1), new Date(2021, 00, 31));
 
 const getClosingPrices = async (dates) => {
     let data = [];
-    let response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-01-01&end=2021-01-03');
+    let response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-01-01&end=2021-01-31');
     let responseData = await response.json();
 
     for (let key in responseData.bpi) {
@@ -28,51 +28,39 @@ const getClosingPrices = async (dates) => {
     return data;
 };
 
-const dataone = getClosingPrices(dateRange);
-console.log('dataone: ', dataone);
+getClosingPrices(dateRange)
+    .then((res) => {
+        const ctx = document.getElementById('myChart').getContext('2d');
 
-const ctx = document.getElementById('myChart').getContext('2d');
-
-const data = {
-    labels: dateRange,
-    datasets: [{
-        fill: false,
-        label: 'Closing Prices',
-        data: getClosingPrices(dateRange),
-        // data: [1, 2, 3],
-        borderColor: 'purple',
-        backgroundColor: 'purple',
-        lineTension: 0,
-    }]
-}
-const options = {
-    type: 'line',
-    data: data,
-    options: {
-        fill: false,
-        responsive: true,
-        scales: {
-            xAxes: [{
-                // type: 'time',
-                type: 'timeseries',
-                // display: true,
-                // scaleLabel: {
-                //     display: true,
-                //     labelString: "Date",
-                // }
+        const data = {
+            labels: dateRange,
+            datasets: [{
+                fill: false,
+                label: 'Closing Prices',
+                data: res,
+                borderColor: 'purple',
+                backgroundColor: 'purple',
+                lineTension: 0,
             }]
-            // yAxes: [{
-            //     ticks: {
-            //         beginAtZero: true,
-            //     },
-            //     display: true,
-            //     scaleLabel: {
-            //         display: true,
-            //         labelString: "Closing Price",
-            //     }
-            // }]
         }
-    }
-}
-const chart = new Chart(ctx, options);
+        const options = {
+            type: 'line',
+            data: data,
+            options: {
+                fill: false,
+                responsive: true,
+                scales: {
+                    y: {
+                        ticks: {
+                            callback: (value, index, values) => {
+                                return `${value} (BTC)`;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        const chart = new Chart(ctx, options);
+    })
+
 
