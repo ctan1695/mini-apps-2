8cfc7e64f48a4e262060e11f4d12ddb41d2134dc
Chloe Tan <chloe.tan95@gmail.com>
2021-05-30 22:47:25 -0400

    CmpComplete BMR

--- challenge_1/client/components/searchResults.jsx
+++ challenge_1/client/components/searchResults.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 const SearchResults = (props) => {
   let display = props.results.map((result) => {
+    let dateText = '';
+
+    if (result.date < 0) {
+      dateText = `In ${Math.abs(result.date)} B.C.: `;
+    } else if (result.date.includes('/')) {
+      dateText = `On ${result.date}: `;
+    } else {
+      dateText = `In the year ${result.date}: `;
+    }
+
     return (
       <div>
-        <div>{result.description}</div>
+        <span><b>{dateText}</b></span>
+        <span>{result.description}</span>
         <br></br>
       </div>
       )
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
-const port = 3000;
+const port = 5500;
 const path = require('path');
 const distPath = `${__dirname}/public`;
 
